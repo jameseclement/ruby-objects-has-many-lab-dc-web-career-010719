64361be3e857b428fbb973e1b17c4bc430a09d9a
James Clement <jameseclement@gmail.com>
2019-01-11 03:52:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,19 +12,9 @@ class Author
     @posts = []
   end 
   
-  def add_song(song)
-    self.songs << song 
-    song.artist = self
-    @@song_count += 1
-  end 
-  
-  def add_song_by_name(song_name)
-    self.add_song(Song.new(song_name))
+  def add_post(post)
     
-  end
-  
-  def self.song_count 
-    @@song_count
-    end
-
-end 
+ end
+   
+   
+ end
