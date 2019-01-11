23c31e6d7c66aedc420d4a14eb7c3f2593751efd
James Clement <jameseclement@gmail.com>
2019-01-11 03:46:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,10 +5,10 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   
-  
+  @@songs = []
   def initialize (name)
     @name = name
-    @songs = []
+    
   end 
   
   def add_song(song)
