e9881417588a8ea5ec8f21425fa3d672e3ac6b78
James Clement <jameseclement@gmail.com>
2019-01-11 04:11:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,20 @@ class Song
   
   def initialize (name)
   @name = name
+  @@all << self
   end 
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if !self.author.nil?
+      self.author.name
+    else
+      nil
+    end
+   
+  end
   
 end
