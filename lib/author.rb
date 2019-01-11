0e15ec2071bdfcf5af31b1462f76e323264f5570
James Clement <jameseclement@gmail.com>
2019-01-11 03:50:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,11 +5,11 @@ class Author
   attr_accessor :name
   attr_reader :songs
   
-  @@song_count = 0
+  @@post_count = 0
   
   def initialize (name)
     @name = name
-    @songs = []
+    @posts = []
   end 
   
   def add_song(song)
